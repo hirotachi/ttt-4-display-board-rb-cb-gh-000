13169fbb81064b7f1e213960315aa9063166c803
Said Oudouane <milesfiles0@gmail.com>
2018-07-20 20:38:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,5 @@ def display_board(board)
   puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-" * 11
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
-  
+end
+display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "])
