f9d658bd8f3dfb512a1145de41214fea00431bdd
Said Oudouane <milesfiles0@gmail.com>
2018-07-20 20:28:10 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,9 @@
+def tic(number)
+  board = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
+  puts "#{board[0]}|#{board[1]}| #{board[2]}"
+  puts "-" * 11
+  puts "#{board[3]}|#{board[4]}| #{board[5]}"
+  puts "-" * 11
+  puts "#{board[6]}|#{board[7]}| #{board[8]}"
+end 
+  
